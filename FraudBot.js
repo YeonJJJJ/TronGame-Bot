68eff90454d3b3d42e9950b11b2c5b15a9987b79
Lilou R. <lilouroisin2@gmail.com>
2025-12-15 12:13:49 +0100

    Level 3 Version 2

--- FraudBot.js
+++ FraudBot.js
@@ -1,67 +1,85 @@
-function arenaToMatrix(arena) {
-  const size = arena.gridSize;
-  const matrix = [];
-
-  for (let y = 0; y < size; y++) {
-    matrix[y] = [];
-    for (let x = 0; x < size; x++) {
-
-      if (x === y) {
-        matrix[y][x] = 10;  
-      }
-      else if (x === y - 1 || x === y + 1) {
-        matrix[y][x] = 5;  
-      }
-      else {
-        matrix[y][x] = 0;   
-      }
-    }
-  }
-  return matrix;
-}
-
 class FraudBot {
   constructor(name, linkedBike) {
     this.name = name;
     this.linkedBike = linkedBike;
+    this.matrix = [];
+    this.direction = null; 
   }
 
-  getMove(arena) {
+  createMatrix(arena) {
+    const size = arena.gridSize;
+    const { x, y } = this.linkedBike;
+
+    if (!this.direction) {
+      this.direction = (x < size / 2 && y < size / 2)
+        ? "DOWN_RIGHT"   
+        : "UP_LEFT";     
+    }
+
+    this.matrix = [];
+
+    for (let j = 0; j < size; j++) {
+      this.matrix[j] = [];
+      for (let i = 0; i < size; i++) {
+        let isDiagonal = false;
+
+        if (this.direction === "DOWN_RIGHT") {
+          isDiagonal = (i === j);
+        } else {
+          isDiagonal = (i + j === size - 1);
+        }
+
+        if (isDiagonal) {
+          this.matrix[j][i] = 100;
+        } else if (
+          isDiagonal ||
+          (this.direction === "DOWN_RIGHT" && (i === j - 1 || i === j + 1)) ||
+          (this.direction === "UP_LEFT" && (i + j === size - 2 || i + j === size))
+        ) {
+          this.matrix[j][i] = 30;
+        } else {
+          this.matrix[j][i] = 0;
+        }
+      }
+    }
+    return this.matrix;
+  }
+
+  getMove(arena, game) {
     const x = this.linkedBike.x;
     const y = this.linkedBike.y;
 
-    const legalMoves = arena.getLegalMoves(x, y, false);
+    const enemy = game.getOtherPlayer().linkedBike;
 
-    if (!legalMoves || legalMoves.length === 0) {
+    const moves = arena.getLegalMoves(x, y, true);
+    const safeMoves = moves.filter(m => !m.collision);
+
+    if (safeMoves.length === 0) {
       return [x, y];
     }
 
-    const matrix = arenaToMatrix(arena);
-
-    for (const move of legalMoves) {
-      if (matrix[move.yMove][move.xMove] === 10) {
-        return [move.xMove, move.yMove];
-      }
-    }
+    const matrix = this.createMatrix(arena);
 
     let bestScore = -Infinity;
     let bestMove = null;
 
-    for (const move of legalMoves) {
-      const available = arena.getAvailableTilesNumber(move.xMove, move.yMove);
+    for (const move of safeMoves) {
+      const spaceScore = arena.getAvailableTilesNumber(
+        move.xMove,
+        move.yMove
+      );
 
-      if (available > bestScore) {
-        bestScore = available;
-        bestMove = move; 
+      const matrixScore =
+        matrix[move.yMove]?.[move.xMove] || 0;
+
+      const totalScore = spaceScore + matrixScore;
+
+      if (totalScore > bestScore) {
+        bestScore = totalScore;
+        bestMove = move;
       }
     }
 
-    if (!bestMove) {
-      const fallback = legalMoves[0];
-      return [fallback.xMove, fallback.yMove];
-    }
-
     return [bestMove.xMove, bestMove.yMove];
-
   }
 }
