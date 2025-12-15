777fc3a9afd0fb234e84a742bead164903dd0561
Lilou R. <lilouroisin2@gmail.com>
2025-12-15 12:09:03 +0100

    Travail en cours

--- FraudBot.js
+++ FraudBot.js
@@ -1,3 +1,25 @@
+function arenaToMatrix(arena) {
+  const size = arena.gridSize;
+  const matrix = [];
+
+  for (let y = 0; y < size; y++) {
+    matrix[y] = [];
+    for (let x = 0; x < size; x++) {
+
+      if (x === y) {
+        matrix[y][x] = 10;  
+      }
+      else if (x === y - 1 || x === y + 1) {
+        matrix[y][x] = 5;  
+      }
+      else {
+        matrix[y][x] = 0;   
+      }
+    }
+  }
+  return matrix;
+}
+
 class FraudBot {
   constructor(name, linkedBike) {
     this.name = name;
@@ -5,51 +27,41 @@ class FraudBot {
   }
 
   getMove(arena) {
-  const x = this.linkedBike.x;
-  const y = this.linkedBike.y;
+    const x = this.linkedBike.x;
+    const y = this.linkedBike.y;
 
-  const legalMoves = arena.getLegalMoves(x, y, false);
+    const legalMoves = arena.getLegalMoves(x, y, false);
 
-  if (!legalMoves || legalMoves.length === 0) {
-    //console.warn("Aucun mouvement possible !");
-    return [x, y];
-  }
-
-  let bestScore = -Infinity;
-  let bestMoves = []; 
-
-  for (const move of legalMoves) {
-    const available = arena.getAvailableTilesNumber(move.xMove, move.yMove);
-    /*console.log(
-      `Move (${move.xMove}, ${move.yMove}) → ${available} cases disponibles`
-    );*/
-
-    if (available > bestScore) {
-      bestScore = available;
-      bestMoves = [move];
-    } else if (available === bestScore) {
-      bestMoves.push(move);
+    if (!legalMoves || legalMoves.length === 0) {
+      return [x, y];
     }
+
+    const matrix = arenaToMatrix(arena);
+
+    for (const move of legalMoves) {
+      if (matrix[move.yMove][move.xMove] === 10) {
+        return [move.xMove, move.yMove];
+      }
+    }
+
+    let bestScore = -Infinity;
+    let bestMove = null;
+
+    for (const move of legalMoves) {
+      const available = arena.getAvailableTilesNumber(move.xMove, move.yMove);
+
+      if (available > bestScore) {
+        bestScore = available;
+        bestMove = move; 
+      }
+    }
+
+    if (!bestMove) {
+      const fallback = legalMoves[0];
+      return [fallback.xMove, fallback.yMove];
+    }
+
+    return [bestMove.xMove, bestMove.yMove];
+
   }
-
-  if (bestMoves.length === 0) {
-    //console.warn("Aucun move valide, choix aléatoire");
-    const randomMove = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-    return [randomMove.xMove, randomMove.yMove];
-  }
-
-  const chosenMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
-
-  /*console.log(
-    "Meilleur(s) move(s) :",
-    bestMoves,
-    "→ choisi :",
-    chosenMove,
-    "score =",
-    bestScore
-  );*/
-
-  return [chosenMove.xMove, chosenMove.yMove];
-}
-
 }
